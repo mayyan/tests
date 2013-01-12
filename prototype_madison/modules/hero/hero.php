f40466a73e649c0eb091ac608515b22633881e3b
May Yan <mayyan@yahoo.com>
2013-01-11 23:05:35 -0800

    prototype_madison: slider

--- prototype_madison/modules/hero/hero.php
+++ prototype_madison/modules/hero/hero.php
@@ -3,24 +3,21 @@
 function renderHero() {
     $html =<<<HTML
 <div class="mod-hero">
-    <article id=slider>
+    <article class="slider">
 
 		<!-- Slider Setup -->
 
-		<input checked type=radio name=slider id=slide1 />
-		<input type=radio name=slider id=slide2 />
-		<input type=radio name=slider id=slide3 />
-		<input type=radio name=slider id=slide4 />
-		<input type=radio name=slider id=slide5 />
-
+		<input type=radio name=slider id=hero-slide1 checked />
+		<input type=radio name=slider id=hero-slide2 />
+		<input type=radio name=slider id=hero-slide3 />
 
 		<!-- The Slider -->
 
-		<div id=slides>
+		<div class="slides">
 
-			<div id=overflow>
+			<div class="overflow">
 
-				<div class=inner>
+				<div class="inner">
 
 					<article>
 						<div class=info></div>
@@ -37,46 +34,22 @@ function renderHero() {
 						<img src="images/slide3.jpg" />
 					</article>
 
-					<article>
-						<div class=info></div>
-						<img src="images/slide1.jpg" />
-					</article>
-
-					<article>
-						<div class=info></div>
-						<img src="images/slide1.jpg" />
-					</article>
-
 				</div> <!-- .inner -->
 
-			</div> <!-- #overflow -->
+			</div> <!-- .overflow -->
 
-		</div> <!-- #slides -->
+		</div> <!-- .slides -->
 
 
 		<!-- Controls and Active Slide Display -->
 
-		<div id=controls>
+		<div class="controls">
 
-			<label for=slide1></label>
-			<label for=slide2></label>
-			<label for=slide3></label>
-			<label for=slide4></label>
-			<label for=slide5></label>
+			<label for=hero-slide1></label>
+			<label for=hero-slide2></label>
+			<label for=hero-slide3></label>
 
-		</div> <!-- #controls -->
-        <div class="clearfix"></div>
-
-
-		<div id=active>
-
-			<label for=slide1></label>
-			<label for=slide2></label>
-			<label for=slide3></label>
-			<label for=slide4></label>
-			<label for=slide5></label>
-
-		</div> <!-- #active -->
+		</div> <!-- .controls -->
 
 
 	</article> <!-- #slider -->
